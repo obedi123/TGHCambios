5aba85985538d116e6c8586c8be97499fe9735bd
ObedM <obedisai1@hotmail.com>
2019-06-14 19:52:26 -0400

    V3

--- app/src/main/java/com/tablonca/obedimc/tghcambios/MyService.java
+++ app/src/main/java/com/tablonca/obedimc/tghcambios/MyService.java
@@ -1,46 +1,110 @@
 package com.tablonca.obedimc.tghcambios;
 
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Build;
-import android.support.v4.app.NotificationCompat;
 import android.util.Log;
+
+import androidx.core.content.ContextCompat;
+
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
-import static android.support.constraint.motion.utils.Oscillator.TAG;
-
 public class MyService extends FirebaseMessagingService {
+    private static final String NUEVA_TASA = "Nueva Tasa";
+
+    public MyService() {
+    }
+
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
-        Log.d("msg", "onMessageReceived: " + remoteMessage.getData().get("message"));
-        Intent intent = new Intent(this, WebActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
-        String channelId = "Default";
-        NotificationCompat.Builder builder = new  NotificationCompat.Builder(this, channelId)
-                .setSmallIcon(R.drawable.ic_stat_logo2)
-                .setContentTitle("test")
-                .setContentText(remoteMessage.getNotification().getBody()).setAutoCancel(true).setContentIntent(pendingIntent);
-        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel channel = new NotificationChannel(channelId, "Default channel", NotificationManager.IMPORTANCE_DEFAULT);
-            manager.createNotificationChannel(channel);
+
+        if (remoteMessage.getData().size() > 0 && remoteMessage.getNotification() != null) {
+            sendNotification(remoteMessage);
         }
-        manager.notify(0, builder.build());
+    }
+
+    private void sendNotification(RemoteMessage remoteMessage) {
+        String descStr = remoteMessage.getData().get(NUEVA_TASA);
+        float desc = Float.valueOf(descStr != null ? descStr : "0");
+
+
+        Intent intent = new Intent(this, WebActivity.class);
+        intent.putExtra(NUEVA_TASA, desc);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        RemoteMessage.Notification notification = remoteMessage.getNotification();
+        if (notification != null) {
+            Notification.Builder notificationBuilder = new Notification.Builder(this)
+                    .setSmallIcon(R.drawable.ic_stat_name)
+                    .setContentTitle(remoteMessage.getNotification().getTitle())
+                    .setContentText(remoteMessage.getNotification().getBody())
+                    .setAutoCancel(true)
+                    .setSound(defaultSoundUri)
+                    .setContentIntent(pendingIntent);
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                notificationBuilder.setColor(desc > .4 ?
+                        ContextCompat.getColor(getApplicationContext(), R.color.colorPrimary) :
+                        ContextCompat.getColor(getApplicationContext(), R.color.colorAccent));
+            }
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                String channelId = null;
+                if (desc == 1) {
+                    channelId = getString(R.string.peru_channel_id);
+                } else if (desc == 2) {
+                    channelId = getString(R.string.colombia_channel_id);
+                } else if (desc == 3) {
+                    channelId = getString(R.string.ecuador_channel_id);
+                }
+                String channelName = null;
+                if (desc == 1) {
+                    channelName = getString(R.string.peru_channel_name);
+                } else if (desc == 2) {
+                    channelName = getString(R.string.colombia_channel_name);
+                } else if (desc == 3) {
+                    channelName = getString(R.string.ecuador_channel_name);
+                }
+                NotificationChannel channel = new NotificationChannel(channelId, channelName,
+                        NotificationManager.IMPORTANCE_DEFAULT);
+                channel.enableVibration(true);
+                channel.setVibrationPattern(new long[]{100, 200, 200, 50});
+                if (notificationManager != null) {
+                    notificationManager.createNotificationChannel(channel);
+                }
+
+                notificationBuilder.setChannelId(channelId);
+            }
+
+            if (notificationManager != null) {
+                notificationManager.notify("", 0, notificationBuilder.build());
+            }
+        }
     }
 
     @Override
     public void onNewToken(String token) {
-        Log.d(TAG, "Refreshed token: " + token);
-
-        // If you want to send messages to this application instance or
-        // manage this apps subscriptions on the server side, send the
-        // Instance ID token to your app server.
-        //sendRegistrationToServer(token);
+        super.onNewToken(token);
+        sendRegistrationToServer(token);
     }
 
+    private void sendRegistrationToServer(String token) {
+        Log.d("new Token", token);
+    }
 }
--- app/src/main/java/com/tablonca/obedimc/tghcambios/NotificationActivity.java
+++ app/src/main/java/com/tablonca/obedimc/tghcambios/NotificationActivity.java
@@ -1,31 +1,97 @@
 package com.tablonca.obedimc.tghcambios;
 
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Intent;
-import android.support.v4.app.NotificationCompat;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class NotificationActivity extends AppCompatActivity {
-    NotificationCompat.Builder mBuilder;
-    int mNotificationId = 001;
+
+    private static final String SP_TOPICS = "sharedPreferencesTopics";
+
+    @BindView(R.id.spTopics)
+    Spinner spTopics;
+    @BindView(R.id.tvTopics)
+    TextView tvTopics;
+
+    private Set<String> mTopicsSet;
+    private SharedPreferences mSharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
+        ButterKnife.bind(this);
 
-        mBuilder = new NotificationCompat.Builder (this)
-                .setSmallIcon(R.drawable.ic_stat_logo2)
-                .setContentTitle("Actualizacion de Tasa")
-                .setContentText("Tasa para Perú: ")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        configSharedPreferences();
 
-        Intent resultIntent = new Intent(this, WebActivity.class);
-        PendingIntent resultPendingIntent = PendingIntent.getActivity(this, 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-        mBuilder.setContentIntent(resultPendingIntent);
-        NotificationManager mNotifyMgr = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        mNotifyMgr.notify(mNotificationId, mBuilder.build());
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnSuccessListener(new OnSuccessListener<InstanceIdResult>() {
+                    @Override
+                    public void onSuccess(InstanceIdResult instanceIdResult) {
+                        Log.d("tokenId", instanceIdResult.getToken());
+                    }
+                });
+    }
+
+    private void configSharedPreferences() {
+        mSharedPreferences = getPreferences(Context.MODE_PRIVATE);
+
+        mTopicsSet = mSharedPreferences.getStringSet(SP_TOPICS, new HashSet<String>());
+
+        showTopics();
+    }
+
+    private void showTopics() {
+        tvTopics.setText(mTopicsSet.toString());
+    }
+
+    @OnClick({R.id.btnSuscribir, R.id.btnDesuscribir})
+    public void onViewClicked(View view) {
+
+        String topic = getResources().getStringArray(R.array.topicsValues)[spTopics.getSelectedItemPosition()];
+
+        switch (view.getId()) {
+            case R.id.btnSuscribir:
+                if (!mTopicsSet.contains(topic)) {
+                    FirebaseMessaging.getInstance().subscribeToTopic(topic);
+                    mTopicsSet.add(topic);
+                    saveSharedPreferences();
+                }
+                break;
+            case R.id.btnDesuscribir:
+                if (mTopicsSet.contains(topic)) {
+                    FirebaseMessaging.getInstance().unsubscribeFromTopic(topic);
+                    mTopicsSet.remove(topic);
+                    saveSharedPreferences();
+                }
+                break;
+        }
+    }
+
+    private void saveSharedPreferences() {
+        SharedPreferences.Editor editor = mSharedPreferences.edit();
+        editor.clear();
+        editor.putStringSet(SP_TOPICS, mTopicsSet);
+        editor.apply();
+
+        showTopics();
     }
 }
